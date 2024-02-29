3cc91ea6f9aeedfc0f83935df0ccdb6164938ed8
Specter-0 <sigurd.thornes@gmail.com>
2024-02-29 13:12:06 +0100

    inital commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,110 @@
-import Image from "next/image";
+"use client"
+import { Suspense, useEffect, useState } from 'react';
+import papa from "papaparse"
+import { motion } from 'framer-motion';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [data, setData] = useState(null);
+    const [questions, setQuestions] = useState([]);
+    const [aindex, setAindex] = useState(0);
+
+    useEffect(() => {
+        fetch("/data.csv").then((response) => {
+            response.text().then((text) => {
+                const result = papa.parse(text, { header: true });
+                setData(result.data);
+                console.log(result.data)
+
+                setQuestions(Object.keys(result.data[0]).filter(value => !(
+                    value.includes("Feedback") || 
+                    value.includes("Points") || 
+                    [
+                        "ID", 
+                        "Start time",
+                        "Name",
+                        "Completion time",
+                        "Total points",
+                        "Quiz feedback",
+                        "Grade posted time",
+                        "Last modified time",
+                        "Email"
+                    ].includes(value)
+                )))
+            });
+        });
+    }, [])
+
+    return (
+        <main className="w-screen overflow-hidden flex flex-col items-center text-white pt-10">
+            <div className='w-2/3 h-[250px] bg-white mb-6 p-6 rounded-lg flex items-center'>
+                <span className='text-2xl italic text-black'>
+                    Naturfag - Kap 5: Arv og Evolusjon
+                </span>
+            </div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <div className="relative w-2/3 flex flex-col items-center bg-white text-black rounded-lg">
+                    {(data && questions) && 
+                        questions.map((question, index) => {
+                            return (
+                                <Block 
+                                    key={index}
+                                    index={index + 1}
+                                    question={question}
+                                    data={data}
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </Suspense>
+        </main>
+    );
+}
+
+function Block({index, question, data}) {
+    const [aindex, setAindex] = useState(0);
+
+    useEffect(() => {
+        console.log(data[aindex], aindex)
+    }, [aindex])
+
+    return (
+        <div className='relative w-full min-h-[200px] px-16 py-8 flex flex-col gap-5'>
+            <span className='text-lg px-1 h-1/3'>
+                {index}. {question}
+            </span>
+            <div className="realtive flex flex-row jusitfy-start w-full h-2/3">
+                <div className='w-1/3 h-32 flex sm:flex-row flex-col gap-4 items-center justify-center pr-10'>
+                    <motion.div className="w-10 h-10"
+                        whileHover={{ scale: 1.06 }}
+                        transition={{ duration: 0.2, ease: "circInOut" }}
+
+                        onClick={() => aindex > 0 && setAindex(prev => prev - 1)}
+                    >
+                        <img src="/arrow_button.svg" alt="previus" className='' />
+                    </motion.div>
+                    <motion.div className="w-10 h-10"
+                        whileHover={{ scale: 1.06 }}
+                        transition={{ duration: 0.2, ease: "circInOut" }}
+
+                        onClick={() => aindex < data.length - 1 && setAindex(prev => prev + 1)}
+                    >
+                        <img src="/arrow_button.svg" alt="next" className='rotate-180' />
+                    </motion.div>
+                </div>
+
+                <div className='w-2/3 text-center'>
+                    {data[aindex][question]}
+                </div>
+            </div>
+
+            <motion.span className='absolute bottom-0 left-1/2 -translate-x-1/2 origin-center w-4/5 h-[2px] bg-gray-500/50'
+                initial={{ width: 0 }}
+                whileInView={{ width: "90%" }}
+                transition={{ duration: 0.5 }}
+            >
+
+            </motion.span>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    )
 }
